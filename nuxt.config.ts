c5bde705dba10bc0fea26183e4243df1f5707783
Manuchehr <manuchekhr.3232@gmail.com>
2025-05-02 19:32:30 +0500

    feat: upgrade tailwindcss v4 & remove nuxt/tailwind module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,17 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  css: ["~/assets/css/tailwind.css"],
   devtools: { enabled: true },
-  modules: ["@pinia/nuxt", "@nuxtjs/tailwindcss", "@nuxt/eslint"],
+  modules: ["@pinia/nuxt", "@nuxt/eslint"],
   runtimeConfig: {
     public: {
       apiBase: process.env.API_BASE,
     },
   },
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  compatibilityDate: "2025-05-02",
 });
